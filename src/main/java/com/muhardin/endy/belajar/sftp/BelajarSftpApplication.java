c296181372b5cf5548bfe01b7435e1c853ae25ab
Endy Muhardin <endy.muhardin@gmail.com>
2021-08-09 13:42:47 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/sftp/BelajarSftpApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.sftp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSftpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSftpApplication.class, args);
+	}
+
+}
